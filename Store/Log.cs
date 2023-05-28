f8912e7fb258adf0950bcd8b55f7be27d5c418e8
azaliia_sagegirl <azaliya-1104@mail.ru>
2023-05-28 22:56:02 +0300

    'working'

---
+++ Store/Log.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+
+namespace StoreASP {
+    public class Log {
+        public string path = "Log.txt";
+        public void WriteLog(string log) {
+            StreamWriter sw = new StreamWriter(path, true);
+            sw.WriteLine(log);
+            sw.WriteLine();
+            sw.Close();
+        }
+    }
+}
